c5d50baf1148b78aaee68b0bfa248d92541c41c9
Alejandro Murakami <68560243+AlejandroMurakami@users.noreply.github.com>
2024-01-15 12:43:14 -0500

    archivos2401

---
+++ website/python.py
@@ -0,0 +1,15 @@
+print("hola, esto es python")
+#comentario entre lineas
+print("segunda linea")
+nombre=input("ingrese su nombre: ")
+telefono=input("ingrese su telefono: ")
+fecha_de_nacimiento=input("ingrese su fecha de nacimiento: ")
+
+puntaje01=input("ingrese puntaje del primer torneo: ")
+Torneo1=puntaje01
+puntaje02=input("ingrese puntaje del segundo torneo: ")
+Torneo2=puntaje02
+
+
+PuntajeTotal=puntaje01+puntaje02
+print("PuntajeTotal")
---
+++ website/script.js
@@ -0,0 +1,6 @@
+console.log("hola mundo")
+
+var precio=350;
+var iva = 21;
+var precioTotal = precio + precio*21/100
+console.log("El precio total es: "+precioTotal);
